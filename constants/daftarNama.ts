ce5c1862d616c415d1216d36854bcca58ebef161
dzikriinzl <dzikriaffa123@gmail.com>
2025-07-31 21:18:26 +0800

    fixxxx_commit

--- constants/daftarNama.ts
+++ constants/daftarNama.ts
@@ -2,18 +2,18 @@
 
 const daftarNama = [
   // 5 nama sebelum urutan stambuk (contoh NIM disesuaikan)
-  { nim: "105841112122", nama: "Aninisa Alfrini" },
-  { nim: "105841112222", nama: "Dzullviana" },
   { nim: "105841112522", nama: "Nurlinda Fitria Rahmadani" },
+  { nim: "105841112622", nama: "Rifki Fadhil" },
   { nim: "105841112722", nama: "Hiswan Halim" },
   { nim: "105841112822", nama: "Isnandar" },
+  { nim: "105841112922", nama: "Raditya" },
 
   // 5 nama setelah urutan stambuk (contoh NIM disesuaikan)
   { nim: "105841113122", nama: "Nisa Natayanti" },
-  { nim: "105841113222", nama: "Muh. Fathur Hidayat" },
-  { nim: "105841113322", nama: "Muh. Rayhan Alghifari" },
-  { nim: "105841113422", nama: "Afil Anugrah" },
-  { nim: "105841113722", nama: "Syahrul Ramadhan" },
+  { nim: "105841113222", nama: "Dian Safitri" },
+  { nim: "105841113322", nama: "Muh. Fathur Hidayat" },
+  { nim: "105841113422", nama: "Muh. Rayhan Alghifari" },
+  { nim: "105841113522", nama: "Afil Anugrah" },
 ];
 
 export default daftarNama;
