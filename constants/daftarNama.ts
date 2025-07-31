2283d635340871064400c20d84aeb18f8dbf023d
dzikriinzl <dzikriaffa123@gmail.com>
2025-07-31 19:57:13 +0800

    fix_commit

--- constants/daftarNama.ts
+++ constants/daftarNama.ts
@@ -1,14 +1,25 @@
 const daftarNama = [
+  //105841112122
   "Aninisa Alfrini",
+  //105841112222
   "Dzullviana",
-  "Nur Linda Fitria Ramadhani",
+  //105841112522
+  "Nurlinda Fitria Rahmadani",
+  //105841112722
   "Hiswan Halim",
+  //105841112822
   "Isnandar",
+
   "Nisa Natayanti",
+  //105841113122
   "Muh. Fathur Hidayat",
+  //105841113222
   "Muh. Rayhan Alghifari",
+  //105841113322
   "Afil Anugrah",
-  "Muhammad Syahrul",
+  //105841113422
+  "Syahrul Ramadhan",
+  //105841113722
 ];
 
 export default daftarNama;
